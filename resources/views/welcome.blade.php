5e6876063ecdab957ae8fcebc226d64daf3fd345
Jebreil Jhon Blancada <jebreilblancada20@gmail.com>
2025-08-25 12:38:42 +0800

    setup github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>SubdiRent Home</title>
+</head>
+
+<body>
+    <h1>Welcome to SubdiRent!</h1>
+    <p>This is a test page for your Laravel Subdivision Rental System.</p>
+</body>
+
+</html>
