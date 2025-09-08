442eb9c4e3a5f81d6a2f269aafdc36eed9f642ed
ikennot <domdomkenneth23@gmail.com>
2025-09-08 15:07:28 +0800

    create migration of booking and its corresponding model

---
+++ app/Models/Booking.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\units;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    //
+
+    protected $fillable = [
+       
+        'property_id',
+        'booking_date',
+        'booking_time',
+        'full_name',
+        'email',
+        'phone_number',
+        'visit_type',
+        'notes'
+        
+    ];
+
+
+    public function property(){
+        return $this->belongsTo(units::class);
+    }
+}
